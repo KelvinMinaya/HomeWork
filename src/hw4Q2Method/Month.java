7f2e39e16ee0a590f5eee17b9cc786f562a00f09
KelvinMinaya <klvnmny@gmail.com>
2021-10-31 09:43:50 -0400

    10312021 9:43pm

---
+++ src/hw4Q2Method/Month.java
@@ -0,0 +1,68 @@
+package hw4Q2Method;
+
+public class Month {
+
+	public static void main(String[] args) {
+
+		String monthOfBirth = "YourMonthOfBirthHere";
+
+		switch ("October") {
+
+		case "January":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "February":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "March":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "April":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "May":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "June":
+			System.out.println("This is your Birthday Month");
+
+			break;
+
+		case "July":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "August":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "September":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "October":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "November":
+			System.out.println("This is your Birthday Month");
+
+			break;
+		case "December":
+			System.out.println("This is your Birthday Month");
+
+			break;
+
+		default:
+			System.out.println("This is not your Birthday Month");
+			break;
+		}
+
+	}
+
+}
