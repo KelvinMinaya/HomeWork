c68655939d91f07b56fdb4e4113585d987a558ec
KelvinMinaya <klvnmny@gmail.com>
2021-11-17 11:04:05 -0500

    11172021 11:03am

---
+++ src/hw6Q3Abstraction/ColumbiaUniversity.java
@@ -0,0 +1,14 @@
+package hw6Q3Abstraction;
+
+public class ColumbiaUniversity {
+//public abstract void chemistry(); -- Abstract method can not be defined in a regular class
+
+	public ColumbiaUniversity() {
+		System.out.println("This is from the constructor");
+	}
+
+	public void biology() {
+		System.out.println("Biology is a great way to learn about the human body");
+	}
+
+}
---
+++ src/hw6Q3Abstraction/NYUniversity.java
@@ -0,0 +1,15 @@
+package hw6Q3Abstraction;
+
+public class NYUniversity {
+
+	// public abstract void anthropology(); -- Abstract method can not be defined in a regular class
+	
+
+	public NYUniversity() {
+		System.out.println("This is from NYU constructor");
+	}
+
+	public void physics() {
+		System.out.println("This class is about physics");
+	}
+}
