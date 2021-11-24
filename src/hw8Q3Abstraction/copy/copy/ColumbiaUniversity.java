4d064be21463211dafea1e15d3abde0c598edbeb
KelvinMinaya <klvnmny@gmail.com>
2021-11-24 00:19:41 -0500

    112321 11:30pm

--- src/hw8Q3Abstraction/copy/copy/ColumbiaUniversity.java
+++ src/hw8Q3Abstraction/copy/copy/ColumbiaUniversity.java
@@ -1,10 +1,11 @@
 package hw8Q3Abstraction.copy.copy;
 
-public class ColumbiaUniversity extends NYUniversity {
-	
+public class ColumbiaUniversity extends EngineeringSchool implements VocationalSchool, University {
+
 	/*
-	 * The extends Keyword is used for inheritance. 1 Key word only and regular class can
-	 * inherit only 1 regular or abstract class
+	 The extends keyword is used for inheritance in Regular class.
+	 * Regular class can inherit "one" regular or abstract class. Keyword "implement"
+	 * used to inherit 1 or more Interface
 	 */
 	
 	
@@ -13,9 +14,21 @@ public class ColumbiaUniversity extends NYUniversity {
 	public ColumbiaUniversity() {
 		System.out.println("This is from the constructor");
 	}
-
+	
 	public void biology() {
 		System.out.println("Biology is a great way to learn about the human body");
+	}
+
+	
+	public void mehcanicalLab() {
+		
+		
+	}
+
+	@Override
+	public void vocationalinfo() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
