f6bc1e4037e84f80fefaf392a00089071ed86450
KelvinMinaya <klvnmny@gmail.com>
2021-11-23 23:14:48 -0500

    112321 11:14pm

--- src/hw8Q2UseOfSuperInChildClass/TestFamilyClass.java
+++ src/hw8Q2UseOfSuperInChildClass/TestFamilyClass.java
@@ -1,17 +1,30 @@
 package hw8Q2UseOfSuperInChildClass;
 
-
 public class TestFamilyClass {
 	public static void main(String[] args) {
-	
-	
-	Father father = new Father();
-	Father father2 = new Father("Jane",25, 'M', false);
-	father.fathers();
-	father.fatherinfo("Carla",27,'F', false);
-	
-	 
-	
+		System.out.println("---------- default constructor----------");
+		Father father = new Father();
+
+		System.out.println("\n---------- parameterized constructor ----------");
+		Father father2 = new Father("Tofael", 483, 'M', false);
+
+		System.out.println("\n---------- void type method ---------");
+		father2.fatherinfo("Kelvin", 25, 'M', false);
+
+		System.out.println("\n---------- parameterized method----------");
+		father2.fatherinfo("Shay", 22, 'F', true);
+
+		System.out.println("\n---------- default constructor----------");
+		Daughter daughter = new Daughter();
+
+		System.out.println("\n---------- parameterized constructor ----------");
+		Daughter daughter2 = new Daughter("Kelly", 20);
+
+		System.out.println("\n---------- void type method ----------");
+		daughter2.daughter();
+
+		System.out.println("\n---------- parameterized method ----------");
+		daughter2.daughterInfo("James", 32);
+
 	}
-	
 }
