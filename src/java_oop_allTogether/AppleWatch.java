9be206f0dccc7e453be70bcbff88bb725939a1ed
KelvinMinaya <klvnmny@gmail.com>
2021-12-14 09:36:14 -0500

    121421 9:36am

---
+++ src/java_oop_allTogether/AppleWatch.java
@@ -0,0 +1,17 @@
+package java_oop_allTogether;
+
+public abstract class AppleWatch {
+	public void abstractClassinfo() {
+		System.out.println("In the abstract class must use the abstract keyword for the abstract method."
+				+ "\nOne regualr class can be extended and more than one interface implemented."
+				+ "\nNon-abstract methods can be implemented."
+				+ "\nAbstract class is a super class that can't create objects."
+				+"\nDefault constructor can be implemented");
+				
+	}
+	public abstract void appleWacthinfo();
+	
+	public AppleWatch() {
+		System.out.println("Abstract class constructor");
+	}
+}
---
+++ src/java_oop_allTogether/AppleWatchSeries5.java
@@ -0,0 +1,16 @@
+package java_oop_allTogether;
+
+public class AppleWatchSeries5 extends AppleWatch implements Watch,DigitalWatch {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+	@Override
+	public void appleWacthinfo() {
+	
+		
+	}
+
+}
