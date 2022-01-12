970e4336760db07ce1f103f0f9b2dcea2ce1cb42
KelvinMinaya <klvnmny@gmail.com>
2022-01-11 21:13:57 -0500

    1/11/22 9:11pm

--- src/htmlSelenium/Assertion.java
+++ src/htmlSelenium/Assertion.java
@@ -1,5 +1,21 @@
-package htmlSelenium;
-
-public class Assertion {
-
-}
+//package practice;
+//
+//import org.testng.Assert;
+//import org.testng.asserts.SoftAssert;
+//
+//public class Assertion {
+//
+//	public void Test1() {
+//
+//		System.out.println("Hard Assertion stops the test after a fail");
+//		Assert.assertEquals(false, true);
+//		System.out.println("This will not be seen unless there's a soft assert");
+//	}
+//
+//	public void Test2() {
+//		System.out.println("Soft Assertion does not stop the test after a fail");
+//		SoftAssert softAssert = new SoftAssert();
+//		softAssert.assertEquals(false, true);
+//		System.out.println("This will not be seen unless there's a soft assert");
+//	}
+//}
